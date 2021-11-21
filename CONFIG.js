942a30a86fff24badca5e4b4cdda980b04c81f00
thinhido124 <86718925+thinhido124@users.noreply.github.com>
2021-11-21 13:23:23 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "nhỏ mày zang hồ lắm phải k",
+  titleHeader: "small mày zang hồ lắm phải k",
   descriptionHeader:
-    "mày zang hồ là tao đấm mài ấy hiền lại 1 xíu ik ",
+    "mày zang hồ I just punched the hole a little bit more ik ",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! mơ à con.",
-  titleModar: "vcl đã zang hồ lại còn hút đá ",
+  titleModar: "hihi zang hồ dễ thương",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> hihi nhỏ hút đá zang hồ cute ^^ </br> "nhỏ zang hồ cute :))) "',
+    'zang hồ ngáo dw</br> hihi nhỏ hút đá zang hồ cute ^^ </br> "nhỏ zang hồ cute :))) "',
 };
