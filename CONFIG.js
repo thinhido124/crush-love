2948e488773c32cd30ea14f31e1ef2c148b28d3e
thinhido124 <86718925+thinhido124@users.noreply.github.com>
2022-01-15 23:06:06 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,11 +1,11 @@
 const  CONFIGDATA  =  {
-  titleHeader : " 𝑒̂𝑦 𝑛ℎ𝑜̉ 𝑚𝑎̀𝑦 𝑧𝑎𝑛𝑔 ℎ𝑜̂̀ 𝑙𝑎̆́𝑚 𝑑𝑢́𝑛𝑔 𝑘 " ,
+  titleHeader : " cj ơi em lỡ thích chị rồi thì sao ạ !? cj có đồng ý làm ny em k ạ :((( " ,
   descriptionHeader :
-    " ℎ𝑖𝑒̂̀𝑛 𝑙𝑎̣𝑖 1 𝑥𝑖́𝑢 𝑖𝑘 𝑙𝑎̀ 𝑐𝑜́ 𝑛𝑦 ^^" ,
+    " iu quá quá rồi cơ :((( ^^" ,
   nút Có : "Đồng Ý" ,
   buttonNo : " Mơ à con " ,
-  titleModar : " 𝑢́𝑖 𝑠𝑎𝑜 𝑐ℎ𝑜̣𝑛 𝑑𝑜̂̀𝑛𝑔 𝑦́ " ,
+  titleModar : " úi sao cj đồng ý " ,
   mô tảModar :
-    ' 𝑇𝑎𝑜 𝑏𝑡 ​​𝑚𝑎̀𝑖 𝑠𝑒̃ 𝑐ℎ𝑜̣𝑛 𝑑𝑜̂̀𝑛𝑔 𝑦́ 𝑚𝑎̀   ^^ </br> 𝑛ℎ𝑜̉ 𝑛𝑔𝑎́𝑜 𝑑𝑎́ 𝑧𝑎𝑛𝑔 ℎ𝑜̂̀ 𝑐𝑢𝑡𝑒 :)) </br> " 𝑛ℎ𝑜̉ 𝑛𝑔𝑎́𝑜 𝑑𝑎́ 𝑧𝑎𝑛𝑔 ℎ𝑜̂̀ 𝑑𝑒̂̃ 𝑡ℎ𝑢̛𝑜̛𝑛𝑔 . </br>   𝑎 ℎ𝑖ℎ𝑖 "' ,
+    'Em biết chị sẽ đồng ý mà   ^^ </br> cj iu cute ghê  </br> " iu cj  hihi. </br>   𝑎 ℎ𝑖ℎ𝑖 "' ,
 } ;
  
